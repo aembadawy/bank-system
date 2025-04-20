11f256f8727ba3acfe093f71cec6e965856ac256
aembadawy <aem.badawy@gmail.com>
2025-04-20 21:37:26 +0200

    Initial Commit

---
+++ Bank-Account/Bank_AccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bank_AccountApp.swift
+//  Bank-Account
+//
+//  Created by Aya on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Bank_AccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
