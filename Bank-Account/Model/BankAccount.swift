cbe95357123dcf39a11dedaea2cfa4c05f333a14
aembadawy <aem.badawy@gmail.com>
2025-04-21 21:16:57 +0200

    Add bank account model

--- Bank-Account/Model/BankAccount.swift
+++ Bank-Account/Model/BankAccount.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+enum BankAccountError {
+    case insufficentFunds
+    case sameAccountTransfer
+    case fruadAlert
+    case invalidAmount
+}
+
+class BankAccount {
+    let owner: String
+    let accountNumber: String
+    var balance: Double
+    
+    init(owner: String, accountNumber: String, balance: Double) {
+        self.owner = owner
+        self.accountNumber = accountNumber
+        self.balance = balance
+    }
+    
+    func deposite(_ amount: Double) {
+        
+    }
+    
+    func withdraw(_ amount: Double) {
+        
+    }
+    
+    func transfer(_ amount: Double, to account: BankAccount) {
+        
+    }
+}
