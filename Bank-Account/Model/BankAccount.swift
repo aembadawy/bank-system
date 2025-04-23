07e4bb336827c92ae842090eaaf1736d4f85cdb0
aembadawy <aem.badawy@gmail.com>
2025-04-23 21:39:54 +0200

    Add bank account functionality

--- Bank-Account/Model/BankAccount.swift
+++ Bank-Account/Model/BankAccount.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum BankAccountError {
+enum BankAccountError: Error {
     case insufficentFunds
     case sameAccountTransfer
     case fruadAlert
@@ -19,21 +19,31 @@ class BankAccount {
     let accountNumber: String
     var balance: Double
     
+    private let fruadThreshold: Double = 5000
+    
     init(owner: String, accountNumber: String, balance: Double) {
         self.owner = owner
         self.accountNumber = accountNumber
         self.balance = balance
     }
     
-    func deposite(_ amount: Double) {
-        
+    func deposite(_ amount: Double) throws {
+        guard amount > 0 else { throw BankAccountError.invalidAmount }
+        balance += amount
     }
     
-    func withdraw(_ amount: Double) {
-        
+    func withdraw(_ amount: Double) throws {
+        guard amount > 0 else { throw BankAccountError.invalidAmount }
+        if balance - amount < 0 { throw BankAccountError.insufficentFunds }
+        if amount >= fruadThreshold { throw BankAccountError.fruadAlert }
+        balance -= amount
     }
     
-    func transfer(_ amount: Double, to account: BankAccount) {
-        
+    func transfer(_ amount: Double, to receiverAccount: BankAccount) throws {
+        guard accountNumber != receiverAccount.accountNumber else {
+            throw BankAccountError.sameAccountTransfer
+        }
+        try self.withdraw(amount)
+        try receiverAccount.deposite(amount)
     }
 }
