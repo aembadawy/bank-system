849cc17b6f13c51b571d2e4816022627c63a9cd7
aembadawy <aem.badawy@gmail.com>
2025-04-25 17:40:28 +0300

    Add swift testing

---
+++ Bank-AccountTests/Bank_AccountTests.swift
@@ -0,0 +1,16 @@
+//
+//  Bank_AccountTests.swift
+//  Bank-AccountTests
+//
+//  Created by Aya on 25/04/2025.
+//
+
+import Testing
+
+struct Bank_AccountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
